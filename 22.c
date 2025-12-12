0e5bf2031a1b7039c44a5bdee570ca796dbc414a
kr-pruto <dafg4765@gmail.com>
2025-12-12 14:43:06 +0900

    Update 22.c

--- 22.c
+++ 22.c
@@ -10,8 +10,9 @@ struct cafe {
 };
 
 int main(void) {
-	struct cafe a;
-
-
+	struct cafe a[4];
+	printf("--- 2025년 12월 9일 ~ 12월 12일간 카페 매출 산출 ---\n");
+	printf("메뉴 순서: 아메리카노, 카라멜 마끼아또, 카페 라떼\n\n");
+	printf("메뉴별 가격을 입력하세요(예: 1500, 3000, 1000): ");
 
 }
