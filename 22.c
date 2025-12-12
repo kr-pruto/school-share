bd8c2a47fc4c2f976b25ec832775253ce7623842
kr-pruto <dafg4765@gmail.com>
2025-12-12 14:36:38 +0900

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int calculate_daily_revenue(struct a* b);
+
+struct cafe {
+	int menu;
+	int Quan;
+};
+
+int main(void) {
+	struct cafe a;
+
+
+
+}
